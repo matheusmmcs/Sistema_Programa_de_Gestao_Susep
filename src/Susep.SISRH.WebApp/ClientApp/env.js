5e990c517c633b3845fd8a6b2ef70cf10d1daa79
Matheus Campanhã <matheusmmcs@gmail.com>
2023-09-05 20:25:09 -0300

    Alteração no env.js do webapp

--- src/Susep.SISRH.WebApp/ClientApp/env.js
+++ src/Susep.SISRH.WebApp/ClientApp/env.js
@@ -1,13 +1,16 @@
 (function (window) {
   window.__env = window.__env || {};
-window.__env.production = false,
-  window.__env.identityUrl = "https://sispghomolog.ufpi.br:8081/",
-  window.__env.apiGatewayUrl = "https://sispghomolog.ufpi.br:8081/",
-window.__env.appTitle = 'SISPG :: UFPI',
+  window.__env.production = true,
+  window.__env.identityUrl = "/gateway/",
+  window.__env.apiGatewayUrl = "/gateway/",
   window.__env.modo = "normal", // "avancado",
+  window.__env.appTitle = 'SISPG :: UFPI - 1.8',
   window.__env.client = {
       id: "SISGP.Web",
       secret: 'secret',
       scope: "SISGP.ProgramaGestao"
-  }
+  },
+  window.__env.valorPadraoTempoComparecimento = 2,
+  window.__env.valorPadraoTermosUso = "Declaro que estou ciente e de acordo com os termos de adesão e de ciência e responsabilidade conforme a Resolução CAD/UFPI n.º 88/2022.",
+  window.__env.formaParticipacaoPlanoTrabalho = 'SomenteSelecionados'
 }(this));
